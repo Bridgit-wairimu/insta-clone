7c04a4368d10b8dfc84645a8fbcb72c161184ed2
Bridgit-wairimu <kirikabetty@gmail.com>
2021-01-20 01:20:18 +0300

    create the view for the signup form

--- instagram/views.py
+++ instagram/views.py
@@ -31,7 +31,26 @@ def welcome(request):
     }
     return render(request, 'welcome.html', params)
 
-    
+
+def Signup(request):
+	if request.method == 'POST':
+		form = SignupForm(request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			email = form.cleaned_data.get('email')
+			password = form.cleaned_data.get('password')
+			User.objects.create_user(username=username, email=email, password=password)
+			return redirect('index')
+	else:
+		form = SignupForm()
+	
+	context = {
+		'form':form,
+	}
+
+	return render(request, 'signup.html', context)
+
+
 @login_required
 def profile(request):
         
