ee34e46d1886c2976b5c21d18cf8439ff84a48c1
Bridgit-wairimu <kirikabetty@gmail.com>
2021-01-20 16:34:23 +0300

    deployment to heroku

--- heyinsta/settings.py
+++ heyinsta/settings.py
@@ -156,7 +156,6 @@ USE_TZ = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
